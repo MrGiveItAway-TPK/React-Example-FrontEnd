817b84b125e6bd935222aa5f360426690a34c7b3
Munes Bani Fawaz <100308806+MrGiveItAway-TPK@users.noreply.github.com>
2022-09-15 15:22:09 +0300

    Update Welcome.js

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { withAuth0 } from '@auth0/auth0-react';
-
+import "./css/style.css";
 
 class Welcome extends React.Component {
   render() {
     return (
         <div id="welcome">
-      <h1>Welcome to the Movie shelf Library</h1>
-      <h2>Sign in to view your collection</h2>
+      <h1>Personal Movie Library</h1>
+      <h2>Sign In To View Your Collection</h2>
       </div>
     )
   }
