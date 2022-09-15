edea3f1ae76897901836b8f348a0db8dc8b3776a
Munes Bani Fawaz <100308806+MrGiveItAway-TPK@users.noreply.github.com>
2022-09-15 15:40:49 +0300

    styling-and-arranging

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,9 +5,11 @@ import "./css/style.css";
 class Welcome extends React.Component {
   render() {
     return (
-        <div id="welcome">
-      <h1>Personal Movie Library</h1>
-      <h2>Sign In To View Your Collection</h2>
+      <div class="center">
+        <div class="center_div_elements">
+          <h1>Personal Movie Library</h1>
+          <h2>Sign In To View Your Collection</h2>
+        </div>
       </div>
     )
   }
